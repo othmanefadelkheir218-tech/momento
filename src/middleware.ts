81daaceac0845d8bf9b180afe9fcf18f97cfeac0
Zakariya <zakariyazouazou@gmail.com>
2026-01-02 20:01:24 +0100

    updae

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,6 @@ import { routing } from './i18n/routing';
 export default createMiddleware(routing);
 
 export const config = {
-    // Match only internationalized pathnames
-    matcher: ['/', '/(de|en|fr)/:path*']
+    // Match all pathnames except for internal paths
+    matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
 };
